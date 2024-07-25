e1f7be81b2bc765c5bb6855c1839b0d4f7b5d1bc
Rahaf Rashid228 <layarashed619@gmail.com>
2024-07-25 16:28:53 +0300

    blog project updated

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,8 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('blogproject', 'root', '', {
-    host: 'localhost',
+export const sequelize = new Sequelize('freedb_blogproject1', 'freedb_rahaf', 'mguXXvqPV8XCg*M', {
+    host: 'sql.freedb.tech',
+    port:3306,
     dialect: 'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
   });
 
